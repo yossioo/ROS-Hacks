67d03834e6ebafa30196a55383aca950876c04c7
Yossi O <yossiovcharik@gmail.com>
2019-08-26 11:04:13 +0300

    setup.sh fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ printf "${LIGHT_BLUE_TXT}Installing ROS-Hacks${NC}.\n"
 
 echo "source ${PWD}/aliases.sh" >>~/.bashrc
 echo "source ${PWD}/functions.sh" >>~/.bashrc
-echo "source_ws \$(cat \$WS_FILE)" >>~/.bashrc
+echo "get_current_ws" >>~/.bashrc
+echo "source_ws $curr_ws" >>~/.bashrc
 if [[ -f ~/.inputrc ]];then
 	mv ~/.inputrc ~/.inputrc.bak
 fi
