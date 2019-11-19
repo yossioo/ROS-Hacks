cdcfa6cb8da99418822f0552f230ddad8ba46af1
Yossi <yossi@robotican.co>
2019-11-19 11:26:54 +0200

    Comments added to the bashrc lines

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@
 source aliases.sh
 printf "${LIGHT_BLUE_TXT}Installing ROS-Hacks${NC}.\n"
 
+echo "## ROS-HACKS entries ##" >>~/.bashrc
 echo "source ${PWD}/aliases.sh" >>~/.bashrc
 echo "source ${PWD}/functions.sh" >>~/.bashrc
 echo "get_current_ws" >>~/.bashrc
 echo 'source_ws $curr_ws' >>~/.bashrc
+echo "## ROS-HACKS END ##" >>~/.bashrc
 if [[ -f ~/.inputrc ]];then
 	mv ~/.inputrc ~/.inputrc.bak
 fi
